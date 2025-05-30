0893cdff59b7b2d5c1e3a0893983a2d465cd5627
Hyperkongj <mosharma0599@gmail.com>
2025-05-29 23:44:24 -0500

    Update sort.java

--- Arrays&Hashing/Problem4_49_GroupAnagrams/sort.java
+++ Arrays&Hashing/Problem4_49_GroupAnagrams/sort.java
@@ -1,7 +1,7 @@
 package Problem4_49_GroupAnagrams;
 import java.util.*;
 
-public class sort {
+public class Sort {
 
     public static List<List<String>> groupAnagrams(String[] strs) {
         Map<String, List<String>> map = new HashMap<>();
